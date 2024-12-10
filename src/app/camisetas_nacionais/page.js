327998733885050b997a4ffe6c77c866ab71119f
Jefferson Portela Pereira <jeffersonportela@live.com>
2024-12-09 22:12:51 -0300

    pqp mil

--- src/app/camisetas_nacionais/page.js
+++ src/app/camisetas_nacionais/page.js
@@ -23,7 +23,7 @@ function Moda_nacional() {
     }, [])
 
     return (
-        <div className='grid mb-5 W-100 overFlowContainer'>
+        <div className='grid mb-5 W-100 bg-light overFlowContainer'>
             {data.map(e => {
                 return (
                     <ItemCards nome={e.name} preco={e.preco} img={e.img} grade={e.grade} key={e.cod} ></ItemCards>
